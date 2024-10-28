60a0de63aad7c2b62aad31722930fd0958a73d33
Mazdaratti <vasya.mazdaratti@googlemail.com>
2024-10-28 11:44:40 +0100

    Improved docstrings

--- storage/istorage.py
+++ storage/istorage.py
@@ -3,38 +3,37 @@ from abc import ABC, abstractmethod
 
 class IStorage(ABC):
     """
-    Interface for movie storage operations. This abstract base class
-    defines the methods required for getting, saving, adding, updating,
-    and deleting movies from a storage source.
+        Interface for movie storage operations.
+        Defines the methods required for getting, saving, adding, updating,
+        and deleting movies from a storage source.
     """
 
     @abstractmethod
     def get_movies(self):
         """
-        Abstract method to retrieve movies from storage.
+            Retrieves movies from storage.
 
-        Returns:
-            dict: A dictionary containing movie data.
+            Returns:
+                dict: A dictionary containing movie data.
         """
         pass
 
     @abstractmethod
     def save_movies(self, dict_object: dict):
         """
-        Abstract method to save movies to storage.
+            Saves movies to storage.
 
-        Args:
-            dict_object (dict): The dictionary object containing movie data to be saved.
+            Args:
+                dict_object (dict): The dictionary object containing movie data to be saved.
         """
         pass
 
     def add_movie(self, movie: dict):
         """
-        Adds a movie dictionary to the storage database. Loads existing data,
-        appends the new movie, and saves it back to storage.
+            Adds a new movie entry to the storage database.
 
-        Args:
-            movie (dict): The movie dictionary.
+            Args:
+                movie (dict): Dictionary containing movie details (title, rating, etc.).
         """
         movies = self.get_movies()
         movies.update(movie)
@@ -42,10 +41,10 @@ class IStorage(ABC):
 
     def delete_movie(self, title: str):
         """
-        Deletes a movie from the storage database by title.
+            Removes a movie entry from storage by its title.
 
-        Args:
-            title (str): The title of the movie to be deleted.
+            Args:
+                title (str): The title of the movie to be deleted.
         """
         movies = self.get_movies()
         if title in movies:
@@ -54,11 +53,11 @@ class IStorage(ABC):
 
     def update_movie(self, title: str, notes: str):
         """
-        Updates the specified movie with new notes.
+            Updates the specified movie with additional notes.
 
-        Args:
-            title (str): The title of the movie to be updated.
-            notes (str): The notes to add to the movie.
+            Args:
+                title (str): The title of the movie to be updated.
+                notes (str): The notes to add to the movie.
         """
         movies = self.get_movies()
         if title in movies:
