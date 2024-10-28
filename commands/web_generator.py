68d60f9d0c86d5ce35263da910005d8341b7a276
Mazdaratti <vasya.mazdaratti@googlemail.com>
2024-10-28 17:21:04 +0100

    WebGenerator added

---
+++ commands/web_generator.py
@@ -0,0 +1,129 @@
+import os
+from pathlib import Path
+
+
+class WebGenerator:
+    """
+        A class to generate a website for displaying movies.
+
+        This class loads an HTML template, serializes movie data,
+        and generates an HTML file with a movie grid based on the data.
+    """
+
+    project_dir = Path(__file__).parent.parent
+    template_path = os.path.join(project_dir,
+                                 "_static", "index_template.html")
+    new_index_path = os.path.join(project_dir,
+                                  "_static", "index.html")
+
+    def __init__(self, movies_data, new_path=new_index_path):
+        """
+            Initialize the WebGenerator with movie data and an output path.
+
+            Args:
+                movies_data: The data containing movie information.
+                new_path (str, optional): The path to save the generated HTML file.
+                                          Defaults to `new_index_path`.
+        """
+        self.movies = movies_data
+        self.new_path = new_path
+
+    def generate_website(self) -> None:
+        """
+            Generate the HTML website for the movie list.
+
+            This method loads the template, inserts serialized movie data into the
+            template, and writes the updated HTML to the specified output path.
+        """
+
+        template = self.load_template(WebGenerator.template_path)
+        serialized_data = self.serialize_movies()
+        new_html = template.replace('__TEMPLATE_MOVIE_GRID__', serialized_data)
+        self.write_file(self.new_path, new_html)
+
+    @staticmethod
+    def load_template(path: str) -> str:
+        """
+            Load and return the content of a template file.
+
+            Opens the specified file in read mode, reads its content, and returns it.
+            Raises an error if the file is not found or if it's empty.
+
+            Args:
+                path (str): The path to the file.
+
+            Returns:
+                str: The content of the file as a string.
+
+            Raises:
+                FileNotFoundError: If the file does not exist.
+                ValueError: If the file is empty.
+        """
+        try:
+            with open(path, "r", encoding="utf-8") as file:
+                content = file.read()
+                if not content:
+                    raise ValueError(f"Error: The file '{path}' is empty.")
+                return content
+        except FileNotFoundError:
+            raise FileNotFoundError(f"Error: The file '{path}' was not found.")
+
+    def serialize_movies(self) -> str:
+        """
+            Serialize the movie data into an HTML list format.
+
+            This method generates HTML list items for each movie with details such
+            as title, year, rating, and notes. If notes are present, they are added
+            as a tooltip.
+
+            Returns:
+                str: An HTML string representing the movie list.
+        """
+        output = ''
+        movies = self.movies.get_movies()
+        for movie, details in movies.items():
+            imdb_link = details.get('IMDB Link', '#')
+            poster_url = details.get('Poster', '')
+            year = details.get('Year', 'N/A')
+            rating = details.get('Rating', 'N/A')
+            notes = details.get('Notes', '')
+            # Conditionally add the tooltip span if there are notes
+            notes_html = f'<span class="tooltiptext">{notes}</span>' if notes else ''
+
+            output += f"""
+            <li>
+                <div class="movie">
+                    <a href="{imdb_link}" target="_blank">
+                        <img src="{poster_url}" alt="{movie}" class="movie-poster"/>
+                    </a>
+                    <div class="movie-title">{movie}</div>
+                    <div class="movie-year">({year})</div>
+                    <div class="movie-rating">Rating: {rating}</div>
+                    <div class="movie-notes">{notes_html}</div>
+                </div>
+            </li>
+            """
+
+        return output
+
+    @staticmethod
+    def write_file(path, content):
+        """
+            Write the given content to a file.
+
+            Opens the specified file in write mode, writes the provided content,
+            and prints a success message.
+
+            Args:
+                path (str): The file path where content should be saved.
+                content (str): The content to write to the file.
+
+            Raises:
+                IOError: If there is an issue with writing to the file.
+        """
+        try:
+            with open(path, "w", encoding="utf-8") as file:
+                file.write(content)
+            print(f"Website was successfully generated at {path}.")
+        except IOError as e:
+            print(f"Failed to write to file '{path}': {e}")
--- movie_app.py
+++ movie_app.py
@@ -1,14 +1,45 @@
 import sys
 from commands.analytics import Analytics
 from commands.crud import Crud
+from commands.web_generator import WebGenerator
 
 
 class MovieApp:
+    """
+        A console-based application for managing a movie database, providing a menu interface
+        for CRUD operations, analytics, and web generation.
+
+        This class serves as the main entry point for interacting with a movie database,
+        supporting operations to list, add, delete, and update movies, as well as analytics functions
+        like statistics, sorting, filtering, and generating a website view of the movie database.
+
+        Attributes:
+            _storage (object): An object that interfaces with the movie database storage, such as
+                               a JSON or database file.
+            _crud (Crud): An instance of the Crud class for handling create, read, update, and delete operations.
+            _analytics (Analytics): An instance of the Analytics class for data analysis and statistics.
+            _webgenerator (WebGenerator): An instance of the WebGenerator class for generating a website.
+            menu_entries (list): A list of tuples, each containing a description and a function reference for
+                                 menu options available to the user.
+
+        Methods:
+            exit_command(): Terminates the application.
+            display_menu(): Prints the available menu options.
+            get_user_choice(): Prompts the user for a menu selection and validates the input.
+            run(): Starts the application loop, displaying the menu and executing the chosen commands.
+    """
 
     def __init__(self, storage):
+        """
+            Initializes the MovieApp instance with provided storage and sets up dependencies.
+
+            Args:
+                storage (object): The storage backend for the movie database, providing methods to manage movies.
+        """
         self._storage = storage
         self._crud = Crud(self._storage)
         self._analytics = Analytics(self._storage)
+        self._webgenerator = WebGenerator(self._storage)
         self.menu_entries = [
             ("Exit", self.exit_command),
             ("List movies", self._crud.list_movies),
@@ -21,21 +52,25 @@ class MovieApp:
             ("Movies sorted by rating", self._analytics.sorted_by_rating),
             ("Movies sorted by year", self._analytics.sorted_by_year),
             ("Filter movies", self._analytics.filtered_movies),
-            ("Generate website", self._generate_website)
+            ("Generate website", self._webgenerator.generate_website)
         ]
 
     @staticmethod
     def exit_command():
         """
-        Exits the program by printing a farewell message and terminating the process.
+            Exits the program by printing a farewell message and terminating the process.
+
+            This method is called when the user selects the "Exit" option from the menu.
         """
         print("Bye!")
         sys.exit(0)
 
     def display_menu(self):
         """
-        Displays the menu to the user.
+            Displays the available menu options to the user.
 
+            This method iterates through the `menu_entries` list and prints each option
+            with its corresponding index number.
         """
         print("\nMenu:")
         for index, (description, _) in enumerate(self.menu_entries):
@@ -44,10 +79,14 @@ class MovieApp:
 
     def get_user_choice(self):
         """
-        Prompts the user to select a menu option and ensures valid input.
+            Prompts the user to select a menu option and ensures valid input.
 
-        Returns:
-            int: The index of the chosen menu entry, if valid or None, if invalid
+            Returns:
+                int: The index of the chosen menu entry, if valid; None if invalid.
+
+            This method accepts the user's choice, validates that it is within the range
+            of available options, and returns the choice if valid. If invalid, an error
+            message is displayed and None is returned.
         """
         user_input = input(f"Enter choice (0-{len(self.menu_entries) - 1}): ").strip()
         if user_input:
@@ -60,11 +99,14 @@ class MovieApp:
         print(f"Invalid choice: [{user_input}]")
         return None
 
-    def _generate_website(self):
-        pass
-
     def run(self):
-        """Function to run the movie app"""
+        """
+            Starts the main loop of the movie app, displaying the menu and executing selected commands.
+
+            This method continuously displays the menu, prompts the user for a choice, and executes
+            the corresponding function until the user selects "Exit." After each action, it pauses
+            for user input to continue.
+        """
         print("********** My Movies Database **********")
         while True:
             self.display_menu()
